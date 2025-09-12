472896363731775e4c7a22b14743244d8f0917ec
Ilya <shakurov.77@mail.ru>
2025-09-12 11:07:42 +0300

    feat: install shadcn

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,3 +1,5 @@
+import { Button } from '~/components/ui/button';
+
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
@@ -5,7 +7,9 @@ export function Welcome() {
         <header className="flex flex-col items-center gap-9">
           <div className="max-w-[100vw] p-4">Crypto Dashboard</div>
         </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4"></div>
+        <div className="max-w-[300px] w-full space-y-6 px-4">
+          <Button>Click me</Button>
+        </div>
       </div>
     </main>
   );
