95969173faa066a88f2b9cc66df61b80412a4fae
Ilya <shakurov.77@mail.ru>
2025-09-13 11:14:08 +0300

    feat: config prettier

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,11 +3,11 @@ import { Button } from '~/components/ui/button';
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+      <div className="flex min-h-0 flex-1 flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
           <div className="max-w-[100vw] p-4">Crypto Dashboard</div>
         </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
+        <div className="w-full max-w-[300px] space-y-6 px-4">
           <Button>Click me</Button>
         </div>
       </div>
